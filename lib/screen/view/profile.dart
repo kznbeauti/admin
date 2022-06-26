415ffbf7c59b793b4df89641632acd5a2b5f0c3c
Aung Bo Myint <aungbomyint292@example.com>
2022-06-26 20:03:17 +0900

    first commit

---
+++ lib/screen/view/profile.dart
@@ -0,0 +1,365 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:kozarni_ecome/controller/home_controller.dart';
+import 'package:kozarni_ecome/data/constant.dart';
+import 'package:kozarni_ecome/routes/routes.dart';
+
+class ProfileView extends StatelessWidget {
+  const ProfileView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final HomeController controller = Get.find();
+    return Obx(
+      () => !(controller.currentUser.value == null)
+          ? _LoginUser()
+          : ListView(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 50),
+                  child: Image.asset(
+                    "assets/logo.png",
+                    height: 150,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20, top: 30),
+                  child: TextFormField(
+                    controller: controller.phoneState.value
+                        ? controller.verificationController
+                        : controller.phoneController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: controller.phoneState.value
+                          ? 'Enter your code'
+                          : 'Phone Number(+959***)',
+                    ),
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 50,
+                  margin: const EdgeInsets.only(top: 20, right: 20, left: 20),
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(homeIndicatorColor),
+                    ),
+                    onPressed: controller.login,
+                    child: Text(
+                        controller.phoneState.value ? 'Verify' : 'Send Code'),
+                  ),
+                )
+              ],
+            ),
+    );
+  }
+}
+
+class _LoginUser extends StatelessWidget {
+  const _LoginUser({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final HomeController _controller = Get.find();
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: double.infinity,
+            height: 200,
+            padding: EdgeInsets.only(left: 20, right: 20),
+            margin: EdgeInsets.only(top: 20),
+            child: Card(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 20,
+                      bottom: 20,
+                    ),
+                    child: Image.asset(
+                      "assets/logo.png",
+                      width: 100,
+                      height: 100,
+                    ),
+                  ),
+                  //   ),
+                  // ),
+                  Obx(
+                    () => Text(
+                      _controller.currentUser.value?.emailAddress ?? '',//-Email Address Will be Phone Number beacause
+                      //--I didn't change it,TODO: need to change emailAddress to phone number
+                      //--instance variable of AuthUser Object
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  //-----Point For Admin To Test On Their Own----//
+                  Obx(
+                    () => Text(
+                      "Your points: ${_controller.currentUser.value?.points ?? 0}",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          _AdminPanel(),
+          Padding(
+            padding: const EdgeInsets.only(top: 30, bottom: 20),
+            child: Center(
+              child: GestureDetector(
+                onTap: _controller.logout,
+                child: Text(
+                  "Logout",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: homeIndicatorColor,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _AdminPanel extends StatelessWidget {
+  const _AdminPanel({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final HomeController controller = Get.find();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          margin: EdgeInsets.only(top: 40, bottom: 20, left: 20, right: 20),
+          child: Text(
+            "Admin Feature",
+            style: TextStyle(
+              fontSize: 23,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            controller.changeCat("");
+            Get.toNamed(uploadItemScreen);
+          },
+          child: Container(
+            height: 60,
+            margin: EdgeInsets.only(
+              left: 20,
+              right: 20,
+            ),
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Upload Item"),
+                    Icon(Icons.upload),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            Get.toNamed(mangeItemScreen);
+          },
+          child: Container(
+            height: 60,
+            margin: EdgeInsets.only(
+              left: 20,
+              right: 20,
+            ),
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Manage Item"),
+                    Icon(Icons.edit),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            Get.toNamed(advertisementUrl);
+          },
+          child: Container(
+            height: 60,
+            margin: EdgeInsets.only(
+              left: 20,
+              right: 20,
+            ),
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Manage Advertisement"),
+                    Icon(Icons.edit),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            Get.toNamed(categoriesUrl);
+          },
+          child: Container(
+            height: 60,
+            margin: EdgeInsets.only(
+              left: 20,
+              right: 20,
+            ),
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Manage Categories"),
+                    Icon(Icons.edit),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            Get.toNamed(statusUrl);
+          },
+          child: Container(
+            height: 60,
+            margin: EdgeInsets.only(
+              left: 20,
+              right: 20,
+            ),
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Manage Status"),
+                    Icon(Icons.edit),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            Get.toNamed(tagsUrl);
+          },
+          child: Container(
+            height: 60,
+            margin: EdgeInsets.only(
+              left: 20,
+              right: 20,
+            ),
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Manage Tags"),
+                    Icon(Icons.edit),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            Get.toNamed(promotionUrl);
+          },
+          child: Container(
+            height: 60,
+            margin: EdgeInsets.only(
+              left: 20,
+              right: 20,
+            ),
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Manage Promotions"),
+                    Icon(Icons.edit),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            Get.toNamed(purchaseScreen);
+          },
+          child: Container(
+            height: 60,
+            margin: EdgeInsets.only(
+              left: 20,
+              right: 20,
+            ),
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20, right: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("My Orders  🎁"),
+                    CircleAvatar(
+                        backgroundColor: Colors.orange,
+                        minRadius: 20,
+                        maxRadius: 20,
+                        child: Text(
+                          "${controller.purchcasesCashOn().length + controller.purchcasesPrePay().length}",
+                          style: TextStyle(color: Colors.black, fontSize: 14),
+                        )),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
